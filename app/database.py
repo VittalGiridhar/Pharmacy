c71d3e5d65f39cdf84d4a72c25e6192adcc70961
unknown <vittuvittal2003@gmail.com>
2024-03-21 09:39:01 +0530

    newprofile

--- app/database.py
+++ app/database.py
@@ -2,8 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 import psycopg2
+from app.config import settings
 
-POSTGRESQL_URL="postgresql://pharmacy_3dgv_user:i6nnfzm94GgFX1lR1BYqkcJThFZPa32P@dpg-cnhhko7jbltc73a82qkg-a.oregon-postgres.render.com/pharmacy_3dgv"
+POSTGRESQL_URL=f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}'
 
 engine=create_engine(POSTGRESQL_URL)
 SessionLocal=sessionmaker(bind=engine,autoflush=False,autocommit=False)
