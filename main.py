28eca58e7272129038c47b019a8ef995ea24b1d8
unknown <vittuvittal2003@gmail.com>
2024-03-21 10:37:28 +0530

    newprofile

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ from app import models
 from app.database import engine
 from fastapi import FastAPI
 from app import Image2Text,dialogflow
-from fastapi.middleware.cors import CORSMiddleware
-import uvicorn
-from os import getenv 
 
 models.Base.metadata.create_all(bind=engine)
 
@@ -14,16 +11,6 @@ application=FastAPI()
 application.include_router(Image2Text.router)
 application.include_router(dialogflow.router)
 
-
-
-origins=["*"]
-application.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
-if __name__=="__main__":
-    port=int(getenv("PORT",8000))
-    uvicorn.run("app.api:app",host="0.0.0.0",port=port,reload=True)
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run(application, host="0.0.0.0", port=8000)
