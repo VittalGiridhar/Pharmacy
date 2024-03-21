6e6d6616d3d04ebf4f6d1f173f21dc686d1bf7fe
unknown <vittuvittal2003@gmail.com>
2024-03-21 09:56:09 +0530

    newprofile

--- main.py
+++ main.py
@@ -13,4 +13,4 @@ application.include_router(dialogflow.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(application, host="0.0.0.0", port=8000)
+    uvicorn.run(application, host="0.0.0.0", port=8080)
